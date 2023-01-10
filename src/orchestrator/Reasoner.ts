390812a4720235f5bc3036011c9974a1ad7ea34d
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-01-10 13:31:09 +0100

    Adding documentation

--- src/orchestrator/Reasoner.ts
+++ src/orchestrator/Reasoner.ts
@@ -9,14 +9,40 @@ export abstract class Reasoner {
         this.logger = getLogger();
     }
 
+    /**
+     * Abox appender
+     * @param data - A string containing N3 data
+     */
     public abstract aboxAppend(data: string) : void;
 
+    /**
+     * Tbox appender
+     * @param data - A string containing N3 rules
+     */
     public abstract tboxAppend(data: string) : void;
 
+    /**
+     * Run the reasoner
+     * @returns An N3 string containing the result of the inferences
+     */
     public abstract run() : Promise<string>;
 
+    /**
+     * Clean up memory, diskspace, ...
+     */
     public abstract cleanup() : void;
 
+    /**
+     * Reason on an N3 store of triples with zero or more N3 rules descriptions.
+     * This is a convenience method for:
+     *     reasoner.aboxAppend(n3);
+     *     reasoner.tboxAppend(n3_rule);
+     *     reasoner.run()
+     *     reasoner.cleanup()
+     * @param dataStore - An N3 Store containing data
+     * @param rules - An array of zero or more N3 rules
+     * @returns An N3 Store with the reasoning result
+     */
     public async reason(dataStore: Store, rules: string[]): Promise<Store>{
         const n3 = await rdfTransformStore(dataStore, 'text/turtle');
 
