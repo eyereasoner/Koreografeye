d32b503c40b1677e99aa2039aa203abda602cbcc
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-04-04 15:29:23 +0200

    Updating DemoPlugin with configurable verbose settings

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from "./orchestrator/Reasoner";
 export * from "./orchestrator/reasoner/EyeJsReasoner";
 export * from "./orchestrator/reasoner/EyeReasoner";
 export * from "./policy/PolicyPlugin";
-export * from "./policy/plugin/DebugPlugin";
 export * from "./policy/plugin/DemoPlugin";
 export * from "./policy/plugin/SendEmailPlugin";
 export * from "./policy/plugin/SendNotificationPlugin";
--- src/policy/plugin/DemoPlugin.ts
+++ src/policy/plugin/DemoPlugin.ts
@@ -1,14 +1,49 @@
 import * as N3 from 'n3';
+import { rdfTransformStore } from '../../util';
 import { PolicyPlugin , type IPolicyType } from '../PolicyPlugin';
 
 export class DemoPlugin extends PolicyPlugin {
+    showMainStore = false;
+    showPolicyStore = false;
+    showParameters = false;
 
-    public async execute (_mainStore: N3.Store, _policyStore: N3.Store, policy: IPolicyType) : Promise<boolean> {
+    constructor(showMainStore = false, showPolicyStore = false, showParameters = false) {
+        super();
+        this.showMainStore = showMainStore;
+        this.showPolicyStore = showPolicyStore;
+        this.showParameters = showParameters;
+    }
+
+    public async execute (mainStore: N3.Store, policyStore: N3.Store, policy: IPolicyType) : Promise<boolean> {
 
         console.log("*** BOOM ! All rockets are starting :P ***");
         console.log(`This is your ${policy.target} and it works well with ${policy.mainSubject}`);
-        console.log(`Here are the other parameters for this policy:`);
-        console.log(JSON.stringify(policy,null,4));
+        
+        if (this.showMainStore) {
+            console.log("*** Main store contents ***");
+            const text = await rdfTransformStore(mainStore,'text/turtle');
+            console.log(text);
+        }
+
+        if (this.showPolicyStore) {
+            console.log("*** Policy store contents ***");
+            const text = await rdfTransformStore(policyStore,'text/turtle');
+            console.log(text);
+        }
+
+        if (this.showParameters) {
+            console.log("*** Policy parameters ***");
+            console.log(JSON.stringify(policy,null,4));
+        }
+
+        console.log(
+            "**** HELP ***\n" +
+            "Show more or less verbose input by switch on/off the startup paramters\n" +
+            "in config.jsonld for the DemoPlugin.\n\n" +
+            `   \"showMainStore\" : ${this.showMainStore},\n` + 
+            `   \"showPolicyStore\" : ${this.showPolicyStore},\n` +
+            `   \"showParameters\" : ${this.showParameters}\n` 
+        );
         console.log("*** BYE ***");
 
         return true;
