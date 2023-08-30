55b07f4ff62c31dd8a0fe9e694dfffc6f4d075b8
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-08-30 11:32:36 +0200

    Adding missing experimental ordering of policy executions

--- src/pol.ts
+++ src/pol.ts
@@ -16,7 +16,7 @@ import { ComponentsManager } from 'componentsjs';
 
 let pluginConf = './config.jsonld';
 
-program.version('0.4.3')
+program.version('0.4.4')
        .option('-c,--config <file>', 'configuration file')
        .option('-i,--in <directory>','input directory')
        .option('-e,--err <directory>','error directory')
