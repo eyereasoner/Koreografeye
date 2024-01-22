0412dbc9919d302e14437a09bbf4378499e15769
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-01-22 18:59:30 +0100

    Maintenance release

--- src/orch.ts
+++ src/orch.ts
@@ -23,7 +23,7 @@ const POL_MAIN_SUBJECT = 'https://www.example.org/ns/policy#mainSubject';
 const POL_ORIGIN       = 'https://www.example.org/ns/policy#origin';
 let   orchConf : string | undefined = undefined;
 
-program.version('0.4.6')
+program.version('0.4.7')
        .argument('<rules>')
        .option('-c,--config <file>','config file')
        .option('-i,--in <directory>','input directory')
