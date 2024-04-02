c663392f0d152c578250d77452648234f036366f
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-04-02 14:10:48 +0200

    Version up

--- src/orch.ts
+++ src/orch.ts
@@ -23,7 +23,7 @@ const POL_MAIN_SUBJECT = 'https://www.example.org/ns/policy#mainSubject';
 const POL_ORIGIN       = 'https://www.example.org/ns/policy#origin';
 let   orchConf : string | undefined = undefined;
 
-program.version('0.4.9')
+program.version('0.5.0')
        .argument('<rules>')
        .option('-c,--config <file>','config file')
        .option('-i,--in <directory>','input directory')
--- src/pol.ts
+++ src/pol.ts
@@ -16,7 +16,7 @@ import { ComponentsManager } from 'componentsjs';
 
 let pluginConf : string | undefined = undefined;
 
-program.version('0.4.9')
+program.version('0.5.0')
        .option('-c,--config <file>', 'configuration file')
        .option('-i,--in <directory>','input directory')
        .option('-e,--err <directory>','error directory')
